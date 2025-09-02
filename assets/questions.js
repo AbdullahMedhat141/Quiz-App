3a73b7e362ffd6f2de04e2c0c03266e46518409c
AbdullahMedhat141 <134795680+AbdullahMedhat141@users.noreply.github.com>
2025-09-02 12:05:57 +0300

    Added questions List

---
+++ assets/questions.js
@@ -0,0 +1,143 @@
+export const questions = [
+  {
+    answer: null,
+    question:
+      "Which keyword is used to declare a constant variable in JavaScript?",
+    options: ["var", "let", "const", "static"],
+    correctAnswer: 2,
+    points: 10,
+    setAnswer(i) {
+      this.answer = i;
+    },
+    getAnswer() {
+      return this.answer;
+    },
+  },
+  {
+    answer: null,
+    question: "What is the output of: console.log(typeof NaN)?",
+    options: ["number", "NaN", "undefined", "object"],
+    correctAnswer: 0,
+    points: 10,
+    setAnswer(i) {
+      this.answer = i;
+    },
+    getAnswer() {
+      return this.answer;
+    },
+  },
+  {
+    answer: null,
+    question: "Which method converts a JSON string into a JavaScript object?",
+    options: [
+      "JSON.parse()",
+      "JSON.stringify()",
+      "JSON.toObject()",
+      "JSON.convert()",
+    ],
+    correctAnswer: 0,
+    points: 10,
+    setAnswer(i) {
+      this.answer = i;
+    },
+    getAnswer() {
+      return this.answer;
+    },
+  },
+  {
+    answer: null,
+    question: "What will `console.log(2 == '2')` print?",
+    options: ["true", "false", "undefined", "TypeError"],
+    correctAnswer: 0,
+    points: 10,
+    setAnswer(i) {
+      this.answer = i;
+    },
+    getAnswer() {
+      return this.answer;
+    },
+  },
+  {
+    answer: null,
+    question: "Which of the following is NOT a JavaScript data type?",
+    options: ["String", "Boolean", "Float", "Undefined"],
+    correctAnswer: 2,
+    points: 10,
+    setAnswer(i) {
+      this.answer = i;
+    },
+    getAnswer() {
+      return this.answer;
+    },
+  },
+  {
+    answer: null,
+    question: "What will be the value of `let x; console.log(x);`?",
+    options: ["0", "null", "undefined", "NaN"],
+    correctAnswer: 2,
+    points: 10,
+    setAnswer(i) {
+      this.answer = i;
+    },
+    getAnswer() {
+      return this.answer;
+    },
+  },
+  {
+    answer: null,
+    question: "Which operator is used to check both value and type?",
+    options: ["=", "==", "===", "!=="],
+    correctAnswer: 2,
+    points: 10,
+    setAnswer(i) {
+      this.answer = i;
+    },
+    getAnswer() {
+      return this.answer;
+    },
+  },
+  {
+    answer: null,
+    question: "How do you create a function in JavaScript?",
+    options: [
+      "function = myFunction()",
+      "function myFunction()",
+      "create function myFunction()",
+      "def myFunction()",
+    ],
+    correctAnswer: 1,
+    points: 10,
+    setAnswer(i) {
+      this.answer = i;
+    },
+    getAnswer() {
+      return this.answer;
+    },
+  },
+  {
+    answer: null,
+    question: "Which array method adds a new element to the end of an array?",
+    options: ["push()", "pop()", "shift()", "unshift()"],
+    correctAnswer: 0,
+    points: 10,
+    setAnswer(i) {
+      this.answer = i;
+    },
+    getAnswer() {
+      return this.answer;
+    },
+  },
+  {
+    answer: null,
+    question: "What will `console.log([] == false)` output?",
+    options: ["true", "false", "TypeError", "undefined"],
+    correctAnswer: 0,
+    points: 10,
+    setAnswer(i) {
+      this.answer = i;
+    },
+    getAnswer() {
+      return this.answer;
+    },
+  },
+];
